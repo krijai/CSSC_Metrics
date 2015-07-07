9d6fc2fa188ef0bd20178425ede291c097a04f2a
Shashi Kanth <shashibang@gmail.com>
2015-07-07 23:07:39 +0530

    Making first commit to Github

---
+++ post.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+import sys, os
+app = Flask(__name__)
+@app.route('/date',methods=['GET','POST'])
+def formval():
+	if request.method=='POST':
+		dateval = request.form['datepick']
+		return render_template('layout.html',subdate=dateval)
+@app.route('/', methods=['GET','POST'])
+def value():
+	y= u'r618'
+	return render_template('layout.html',y=y)
+@app.route('/radio', methods=['GET','POST'])
+def rad():
+		x= 'roger'
+		return render_template('layout.html',roger=x)
+	
+if __name__== '__main__':
+	app.run(debug=True)
